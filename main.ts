9013fab10b5ddd8c80cd082045a59d60bec65f05
OCSBAnthonyj <113910453+OCSBAnthonyj@users.noreply.github.com>
2022-10-28 09:46:27 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,37 @@
+function RED () {
+    range = strip.range(0, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Red))
+    range = strip.range(1, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(2, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+}
+function GREEN () {
+    range = strip.range(2, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Green))
+    range = strip.range(1, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(0, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+}
+function YELLOW () {
+    range = strip.range(1, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    range = strip.range(0, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(3, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+}
+let range: neopixel.Strip = null
+let strip: neopixel.Strip = null
+basic.showIcon(IconNames.Heart)
+strip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
+strip.setBrightness(70)
+basic.forever(function () {
+    GREEN()
+    basic.pause(10000)
+    YELLOW()
+    basic.pause(5000)
+    RED()
+    basic.pause(10000)
+})
